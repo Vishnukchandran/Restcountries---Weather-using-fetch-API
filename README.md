d5fc61ef44f2d3c06b69c49704c92e94673c95c9
Vishnukchandran <vishnuvkc@gmail.com>
2024-01-31 21:42:36 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Restcountries & Weather using fetch API
+
+This repository has a simple website to display the countries details such as Capital,Flag,Region,Name,country codes and Weather of the respective country from restcountries and openweather API.
+
+Access the live page [here]()
---
+++ script.js
@@ -0,0 +1,65 @@
+var API = "https://restcountries.com/v3.1/all";
+let values;
+var apifetch = fetch(API)
+  .then((response) => response.json())
+  .then((data) => {
+    data.map((value) => {
+      var card = {
+        ...value,
+        name: value.name.common,
+        flag: value.flags.png,
+        code: value.cca3,
+        capital: value.capital,
+        region: value.region,
+        latitude: value.latlng[0],
+        longitude: value.latlng[1],
+      };
+      createcountry(card);
+    });
+  });
+
+function createcountry({
+  name,
+  flag,
+  code,
+  capital,
+  region,
+  latitude,
+  longitude,
+}) {
+  document.body.innerHTML += ` <div class="container d-flex flex-row">
+    <div class=" card"  >
+         <div class="card-header">
+    ${name}
+  </div>
+
+  <div class="card-body" >  
+  <img src="${flag}" class="flag" alt="${name}'Flag image">
+  <p class="card-text"><span>Captial :</span> ${capital}</p>
+  <p class="card-text"><span>Region :</span> ${region}</p>
+  <p class="card-text"><span>Country Code :</span>${code}</p>
+  <a href="#" class="btn btn-primary" onclick=(block(${latitude},${longitude},${region})) >Click for Weather</a>
+ <div id=${region}>   </div>
+  
+ 
+  </div>
+</div>
+</div>
+`;
+}
+
+function block(lat, lng, name) {
+  var WAPI = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=06e423ec0af839c485470951f60c3f6b`;
+  console.log(name);
+
+  fetch(WAPI)
+    .then((response) => response.json())
+    .then((data) => {
+      alert(`            
+     Humidity is ${data.main.humidity}
+     Pressure is ${data.main.pressure}
+     Temperature is ${data.main.temp}`);
+    });
+}
+
+document.addEventListener("click", (event) => event.preventDefault());
